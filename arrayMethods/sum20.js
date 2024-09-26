d0a063683c0379936c3a825b13d33a3a07190769
Keerthi-28joe <rechelravi@gmail.com>
2024-09-26 16:39:52 +0530

    arrayMethods

---
+++ arrayMethods/sum20.js
@@ -0,0 +1,32 @@
+// let numbers=["One","Two","Three","Four"];
+// let str=" ";
+// for(i=numbers.length-1;i>=0;i--){
+//     str+=(numbers[i]);
+// }
+// console.log(str);
+
+// function table(n){
+//     let sum=1;
+//     for(i=1;i<=n;i++){
+//         sum=sum*7;
+       
+//     }
+//     console.log(sum);
+// }
+// table(10)
+
+function num(num){
+    for(i=0;i<=num;i++){
+     
+    }
+if(num%2==0 && num%5==0){
+    console.log("Number is divisible by both 2 and 5");
+}
+else if(num%2==0){
+    console.log("Number is divisible by 2");
+}
+else{
+    console.log("Number is divisible by 5");
+}
+}
+num([10,20,30,40]);
