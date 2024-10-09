538bcfc4de738d5a4d2300f225a37a8448030acb
Keerthi-28joe <rechelravi@gmail.com>
2024-10-09 09:48:26 +0530

    nestedLoop1

---
+++ dom/nestedLoop1.js
@@ -0,0 +1,10 @@
+function printStr(n){
+    for(i=1;i<=n;i++){
+        let star="";
+        for(j=1;j<=i;j++){
+            star=star+"*";
+        }
+        console.log(star);
+    }
+}
+printStr(3);
---
+++ dom/nestedLoop2.js
@@ -0,0 +1,5 @@
+// Print the below pattern of ‘*’ if n = 3
+// *
+// **
+// ***
+
